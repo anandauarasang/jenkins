fc7a80407265320168fc72b921a499594d7dcba1
HP <HP@DESKTOP-GTSM8RN>
2022-12-12 15:56:10 +0530

    third commit

---
+++ jenkins2/src/main/java/jenkins2/jenkins2/App.java
@@ -0,0 +1,13 @@
+package jenkins2.jenkins2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jenkins2/src/test/java/jenkins2/jenkins2/loginTest.java
@@ -0,0 +1,18 @@
+package jenkins2.jenkins2;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class loginTest {
+	@Test
+	public void browser()
+	{
+		//WebDriver driver = new FirefoxDriver();
+		//driver.get("https://krninformatix.com");
+		System.out.println("Fire fox opened");
+		System.out.println("Passed=1");
+		//driver.close();
+	}
+
+}
